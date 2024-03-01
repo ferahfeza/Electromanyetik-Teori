3ca35d78d78f3f47d3c6ccc2bcce4f3bfb431d1e
ferahfeza <acanakoglu@dpu.edu.tr>
2024-03-01 04:01:45 +0300

    Deployed d7804db with MkDocs version: 1.5.3

---
+++ Py Codes/denek.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy import linalg as LA
+
+
+P1 = [2,4,3]
+P2 = [-1,5,6]
+
+D = np.subtract(P2,P1)
+print("D=",D[0],"ax",D[1],"ay",D[0],"az")
+
+D_gen = LA.norm(D)
+print(D_gen)
+
+aD = D / D_gen
+print(aD)
+
